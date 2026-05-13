0cf0c2b4b6bf20308f0110a7fc6411074e36263b
Steven Sagun <sagunsteven@live.com>
2026-05-13 19:11:02 -0400

    Add ngrx store

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,8 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideStore()],
 };
